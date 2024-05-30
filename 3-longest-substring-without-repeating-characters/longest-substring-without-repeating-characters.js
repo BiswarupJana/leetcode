f9dd62941a5f5c4b9680a22834d22cfc4ed8f378
BiswarupJana <72341967+BiswarupJana@users.noreply.github.com>
2024-05-30 12:00:00 +0530

    Time: 76 ms (83.96%) | Memory: 53.6 MB (59.83%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let set= new Set();
+    let left=0;
+    let right =0;
+    let maxLen =0;
+    while(right < s.length){
+        if(!set.has(s[right])){
+            set.add(s[right]);
+            maxLen = Math.max(maxLen, set.size);
+            right++;
+        }else{
+            set.delete(s[left])
+            left++;
+        }
+    }
+    return maxLen;
+};
