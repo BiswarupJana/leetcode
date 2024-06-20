41679b93aadafab850de911e31b8a1a8a5530065
BiswarupJana <72341967+BiswarupJana@users.noreply.github.com>
2024-06-20 10:34:00 +0530

    Time: 68 ms (95.40%) | Memory: 57.5 MB (57.08%) - LeetSync

--- 189-rotate-array/rotate-array.js
+++ 189-rotate-array/rotate-array.js
@@ -4,7 +4,6 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    const l= nums.length
     k = k % nums.length; // Handle cases where k is larger than the array size
   const part = nums.splice(nums.length - k, k); // Remove the last k elements
   nums.unshift(...part);
